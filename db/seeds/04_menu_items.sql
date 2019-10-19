a37c8f1a214ce67411e2a47cd5898af031e5e587
Jake <jakefantin@gmail.com>
2019-10-19 23:31:24 +0000

    seeds for menu_items now with desc, some with images, drinks added

--- db/seeds/04_menu_items.sql
+++ db/seeds/04_menu_items.sql
@@ -10,47 +10,49 @@ INSERT INTO menu_items (title, description, image_url, price, calories, prep_tim
 INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('TRUFFLE FRIES', 'truffle, herbs, grana padano, garlic aioli', 'https://s3-media1.fl.yelpcdn.com/bphoto/OHmkAdPAEn-sCZJDiE72xg/o.jpg', 1025, NULL, 4, 1);
 INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('CHICKEN WINGS', 'louisiana hot, celery, creamy parmesan dip', 'https://s3-media4.fl.yelpcdn.com/bphoto/e0vmA3k5DyMXmKN6ggFj5w/o.jpg', 1575, NULL, 7, 1);
 INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('SPICY CHICKEN', 'sweet chili glaze, crispy wontons, green onions', 'https://static.wixstatic.com/media/9b3c70_353336ccea98474da23e6e33e0d50f19~mv2.jpg/v1/fill/w_308,h_228,al_c,q_80,usm_1.20_1.00_0.01/9b3c70_353336ccea98474da23e6e33e0d50f19~mv2.webp', 1525, NULL, 7, 1);
--- GREENS --
-INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('RAINCOAST GREENS', '', '', 1975, NULL, 11, 2);
-INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('ROCKET SALAD', '', '', 1975, NULL, 13, 2);
-INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('KALE + GRILLED CHICKEN SALAD', '', '', 1875, NULL, 11, 2);
+-- GREENS -- ST to any salad grilled chicken breast, grilled salmon, sauteed prawns
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('RAINCOAST GREENS', 'fresh greens, grilled chicken breast, avocado, tomatoes, feta, spicy pecans, cucumber, egg, lemon-thyme vinaigrette', '', 1975, NULL, 11, 2);
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('ROCKET SALAD', 'created by chef rob feenie. parmesan crusted chicken breast, arugula, baby gem tomatoes, red onion, lemon caper dressing', '', 1975, NULL, 13, 2);
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('KALE + GRILLED CHICKEN SALAD', 'kale, fresh greens, romaine, dijon-lemon vinaigrette, pickled red onions, parmesan crumble', '', 1875, NULL, 11, 2);
 -- BOWLS --
-INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('MODERN BOWL', '', '', 2050, NULL, 12, 3);
-INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('TUNA POKE BOWL', '', '', 2050, NULL, 14, 3);
-INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('CRIPSY TOFU BOWL', '', '', 1925, NULL, 11, 3);
-INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('TERIYAKI CHICKEN RICE BOWL', '', '', 1975, NULL, 13, 3);
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('MODERN BOWL', 'tabbouleh, pineapple salsa, roasted cauliflower and broccoli, tomatoes, cucumber, fresh greens, jasmine rice, miso carrot ginger sauce', '', 2050, NULL, 12, 3); -- ST griled chicken, grilled avacado,, glazed salmon, soy grilled tofu
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('TUNA POKE BOWL', 'sesame ginger ocean wise™ ahi, jasmine rice, mango, cucumber, avocado, edamame, radish, crispy tempura', '', 2050, NULL, 14, 3);
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('CRIPSY TOFU BOWL', '100% plant-based. soy chili tofu, jasmine rice, mango, cucumber, avocado, edamame, radish', '', 1925, NULL, 11, 3);
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('TERIYAKI CHICKEN RICE BOWL', 'bell peppers, broccoli, braised red cabbage, bean sprouts, spicy yogurt', '', 1975, NULL, 13, 3);
 -- CASUAL --
-INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('BAJA FISH TACOS', '', '', 1875, NULL, 8, 4);
-INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('MINI CRISPY CHICKEN SANDWICHES', '', '', 1550, NULL, 11, 4);
-INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('MINI BURGERS', '', '', 1550, NULL, 10, 4);
-INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('ROBS FLATBREAD', '', '', 1625, NULL, 6, 4);
-INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('CHICKEN TENDERS', '', '', 1625, NULL, 4, 4);
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('BAJA FISH TACOS', 'beer battered ocean wise™ cod, shredded cabbage, chipotle aioli, fire roasted tomato salsa, pickled radish, onions and jalapeños, sea salted fries.', '', 1875, NULL, 8, 4); -- ST guac option
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('MINI CRISPY CHICKEN SANDWICHES', 'spicy panko-crusted chicken, swiss cheese, sambal mayo, lettuce, tomato, pickle, onion', '', 1550, NULL, 11, 4);
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('MINI BURGERS', 'certified angus beef®, cheddar, bacon, lettuce, tomato, red pepper relish, dijon mayonnaise', '', 1550, NULL, 10, 4);
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('ROBS FLATBREAD', 'caramelized onions, grape tomatoes, feta, goat cheese, basil pesto, arugula, balsamic glaze', '', 1625, NULL, 6, 4);
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('CHICKEN TENDERS', 'panko-breaded chicken, sea salted fries, honey mustard dip', '', 1625, NULL, 4, 4);
 -- SEAFOOD --
-INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('GRILLED DIJON SALMON', '', '', 2575, NULL, 14, 5);
-INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('THAI RED CURRY + PRAWNS', '', '', 2225, NULL, 14, 5);
-INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('SABLEFISH', '', '', 3975, NULL, 12, 5);
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('GRILLED DIJON SALMON', 'ocean wise™ lois lake steelhead, buttered mashed potatoes, roasted asparagus, soy-dijon butter sauce, cilantro', '', 2575, NULL, 14, 5);
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('THAI RED CURRY + PRAWNS', 'sustainably harvested prawns, peppers, green beans, baby corn, bamboo shoots, jasmine rice, bean sprouts, wontons, cilantro', '', 2225, NULL, 14, 5);
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('SABLEFISH', 'shiitake mushrooms, asparagus, peking duck broth, yukon gold potatoes', '', 3975, NULL, 12, 5);
 -- PASTA --
-INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('BUTTERNIT SQUASH RAVIOLI WITH PRAWNS', '', '', 2825, NULL, 14, 6);
-INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('SPAGHETTINI + KOBE STYLE MEATBALLS', '', '', 2775, NULL, 14, 6);
-INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('PRAWN SPAGHETTINI', '', '', 2100, NULL, 14, 6);
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('BUTTERNIT SQUASH RAVIOLI WITH PRAWNS', 'chef rob feenie’s signature butternut squash and mascarpone ravioli, truffle butter, sautéed jumbo prawns, pine nuts, crispy sage', '', 2825, NULL, 14, 6);
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('SPAGHETTINI + KOBE STYLE MEATBALLS', 'roasted tomato sauce, truffle mushroom cream, fresh ricotta cheese', '', 2775, NULL, 14, 6);
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('PRAWN SPAGHETTINI', 'slow-roasted tomato sauce, chilies, fresh herbs, crostini', '', 2100, NULL, 14, 6);
 -- CHICKEN --
-INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('BLACKENED CREOLE CHICKEN', '', '', 2600, NULL, 15, 7);
-INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('HUNTER CHICKEN', '', '', 2725, NULL, 14, 7);
-INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('THAI RED CURRY + CHICKEN', '', '', 2100, NULL, 14, 7);
--- BURGERS + SANDWICHES --
-INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('THE FEENIE BURGER', '', '', 1975, NULL, 10, 8);
-INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('CHEDDAR BACON BURGER', '', '', 1850, NULL, 11, 8);
-INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('BBQ DUCK CLUBHOUSE', '', '', 1975, NULL, 8, 8);
-INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('GARDENBURGER', '', '', 1750, NULL, 6, 8);
-INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('CAJUN CHICKEN SANDWICH', '', '', 1700, NULL, 8, 8);
-INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('ROBS CRISPY CHICKEN SANDWICH', '', '', 1775, NULL, 8, 8);
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('BLACKENED CREOLE CHICKEN', 'creole butter, cajun spices, buttered mashed potatoes, roasted asparagus', '', 2600, NULL, 15, 7);
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('HUNTER CHICKEN', 'shiitake, portobello, button and crimini mushroom demi-glace, buttered mashed potatoes, green beans', '', 2725, NULL, 14, 7);
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('THAI RED CURRY + CHICKEN', 'peppers, green beans, baby corn, bamboo shoots, jasmine rice, bean sprouts, wontons, cilantro', '', 2100, NULL, 14, 7);
+-- BURGERS + SANDWICHES -- ST cheddar cheese, mushrooms bacon
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('THE FEENIE BURGER', 'smashed certified angus beef®, sautéed mushrooms, aged cheddar, smoked bacon, red relish, mayonnaise, ketchup, mustard', '', 1975, NULL, 10, 8);
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('CHEDDAR BACON BURGER', 'smashed certified angus beef®, aged cheddar, smoked bacon, red relish, mayonnaise, ketchup, mustard', '', 1850, NULL, 11, 8);
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('BBQ DUCK CLUBHOUSE', 'created by chef rob feenie. peking duck, roasted chicken, prosciutto di modena, pecan fruit bread', '', 1975, NULL, 8, 8);
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('GARDENBURGER', 'aged cheddar, red relish, mayonnaise, ketchup, mustard, multigrain bun', '', 1750, NULL, 6, 8);
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('CAJUN CHICKEN SANDWICH', 'aged cheddar, lettuce, tomato', '', 1700, NULL, 8, 8);
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('ROBS CRISPY CHICKEN SANDWICH', 'spicy panko-crusted chicken, swiss cheese, sambal mayo, lettuce, tomato, pickle and onion', '', 1775, NULL, 8, 8);
 -- DESSERTS --
-INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('APPLE GALETTE', '', '', 1000, NULL, 3, 9);
-INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('CHOCOLATE PEANUT BUTTER CRUNCH BAR', '', '', 1000, NULL, 1, 9);
-INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('CARAMEL CHOCOLATE MOUSSE', '', '', 350, NULL, 5, 9);
-INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('WHITE CHOCOLATE CHEESECAKE', '', '', 1000, NULL, 3, 9);
-INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('KEY LIME PIE', '', '', 1000, NULL, 3, 9);
-INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('CHOCOLATE LAVA CAKE', '', '', 1000, NULL, 3, 9);
-INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('PUMPKIN CHEESECAKE', '', '', 950, NULL, 3, 9);
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('APPLE GALETTE', 'flaky puff pastry, caramelized apples, tahitian vanilla ice cream, caramel sauce', '', 1000, NULL, 3, 9);
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('CHOCOLATE PEANUT BUTTER CRUNCH BAR', 'tahitian vanilla ice cream, caramel sauce, crunchy chocolate pearls', '', 1000, NULL, 1, 9);
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('CARAMEL CHOCOLATE MOUSSE', 'warm caramel foam, crunchy sponge toffee, velvety chocolate mousse', '', 350, NULL, 5, 9); -- ST single, double, triple
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('WHITE CHOCOLATE CHEESECAKE', 'chocolate cookie crust, raspberry compote, hand whipped cream', '', 1000, NULL, 3, 9);
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('KEY LIME PIE', 'freshly squeezed key limes, graham cracker crust, hand whipped cream', '', 1000, NULL, 3, 9);
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('CHOCOLATE LAVA CAKE', 'molten dark chocolate cake, tahitian vanilla ice cream', '', 1000, NULL, 3, 9);
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('PUMPKIN CHEESECAKE', 'warm spices, toasted walnuts, caramel sauce, fresh whipped cream. available for a limited time', '', 950, NULL, 3, 9);
 -- DRINKS --
-
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('COKE', NULL, NULL, 200, null, 1, 10);
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('ICE TEA', NULL, NULL, 200, null, 1, 10);
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ('SPRITE', NULL, NULL, 200, null, 1, 10);
