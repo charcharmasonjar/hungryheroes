620b0c3170b047cb2da1bdb0bfdd26f41ea8e15c
Jake <jakefantin@gmail.com>
2019-10-19 21:06:49 +0000

    added seeds for users and courses tables as well as shells for other seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,9 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+-- users table seeds --
+INSERT INTO users (first_name, last_name, email, password, phone) VALUES ('Kira', 'Yagami', 'light.yagami@gmail.com', 'password', '2503458756');
+INSERT INTO users (first_name, last_name, email, password, phone) VALUES ('Sam', 'Fleming', 'sam.fleming@gmail.com', 'password', '2503473756');
+INSERT INTO users (first_name, last_name, email, password, phone) VALUES ('Jaala', 'Deroche', 'jaala.deroche@gmail.com', 'password', '2505819756');
+INSERT INTO users (first_name, last_name, email, password, phone) VALUES ('Beth', 'Aiken', 'beth.aiken@gmail.com', 'password', '250349246');
+INSERT INTO users (first_name, last_name, email, password, phone) VALUES ('Hannah', 'Barrett', 'hannah.barrett@gmail.com', 'password', '2503458823');
+INSERT INTO users (first_name, last_name, email, password, phone) VALUES ('Joe', 'Eaton', 'joe.eaton@gmail.com', 'password', '2503481245');
+INSERT INTO users (first_name, last_name, email, password, phone) VALUES ('Alex', 'Reid', 'alex.reid@gmail.com', 'password', '2503297756');
+
---
+++ db/seeds/04_menu_items.sql
@@ -0,0 +1,2 @@
+-- menu_items table seeds --
+INSERT INTO menu_items (title, description, image_url, price, calories, prep_time, course_id) VALUES ()
