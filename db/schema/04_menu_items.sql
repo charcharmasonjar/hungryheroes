8e582cc9b8084a33393a0b30d339008a2d3af63d
charcharmasonjar <charlotte.dibb@gmail.com>
2019-10-19 20:41:14 +0000

    Fix brackets around create menu_items table

--- db/schema/04_menu_items.sql
+++ db/schema/04_menu_items.sql
@@ -2,7 +2,7 @@
 -- default is that menu items are not on pecial
 DROP TABLE IF EXISTS menu_items;
 
-CREATE TABLE menu_items {
+CREATE TABLE menu_items (
   id SERIAL PRIMARY KEY NOT NULL,
   description TEXT,
   image_url VARCHAR(255),
@@ -10,4 +10,4 @@ CREATE TABLE menu_items {
   calories INTEGER,
   special BOOLEAN NOT NULL DEFAULT FALSE,
   course_id INTEGER REFERENCES courses(id) NOT NULL
-};
+);
