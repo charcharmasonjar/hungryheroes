080126f7ae1116599f0290b39fbbbac93d59db94
charcharmasonjar <charlotte.dibb@gmail.com>
2019-10-19 20:59:58 +0000

    Fix brackets around create courses table

--- db/schema/03_courses.sql
+++ db/schema/03_courses.sql
@@ -1,7 +1,7 @@
 -- drops and creates courses table (apps, mains, desserts, etc)
 DROP TABLE IF EXISTS courses CASCADE;
 
-CREATE TABLE courses {
+CREATE TABLE courses (
   id INTEGER PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL
-};
+);
