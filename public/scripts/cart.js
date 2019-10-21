90db034fe4615c6b61095bb60a4743bd5e471a44
charcharmasonjar <charlotte.dibb@gmail.com>
2019-10-21 23:00:52 +0000

    Prevent default behaviour of submitting a form with the calss add-to-cart

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -1,9 +1,17 @@
 $(document).ready(function () {
 
+  $(".add-to-cart").submit(function (event) {
+    event.preventDefault();
+    console.log("default prevented");
+    // calling loadTweets
+
+  })
+
+
   //toggles write payment form when add payment button is clicked
   $("#add-payment").click(() => {
     $(".payment-container").slideToggle("fast", () => {
-     //done
+      //done
     });
   })
 
