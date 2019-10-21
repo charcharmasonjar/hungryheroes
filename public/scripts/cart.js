12f054e4b0bfc1f1abac709a51fc9b0f613b85e6
charcharmasonjar <charlotte.dibb@gmail.com>
2019-10-21 20:34:21 +0000

    Remove unecessary console log

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
   //toggles write payment form when add payment button is clicked
   $("#add-payment").click(() => {
     $(".payment-container").slideToggle("fast", () => {
-      console.log("done");
+     //done
     });
   })
 
