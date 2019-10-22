b62f4b3f6b0bd621b91de8d53fa3492c3c37518c
charcharmasonjar <charlotte.dibb@gmail.com>
2019-10-22 20:59:58 +0000

    Remove cookie related code - will store cart as a global variable instead

--- routes/cart.js
+++ routes/cart.js
@@ -1,34 +1,14 @@
 /*
  * Since this file is loaded in server.js into /cart,
- * these routes are mounted onto /users
+ * these routes are mounted onto /cart
  */
 
 const express = require('express');
 const cartRoutes = express.Router();
-const cookieSession = require('cookie-session');
-const app = express();
-
-app.set('trust proxy', 1) // trust first proxy
-
-app.use(cookieSession({
-  name: 'session',
-  keys: ['key1', 'key2']
-}))
 
 module.exports = (dbHelpers) => {
   cartRoutes.post("/", (req, res) => {
-    const menuItem = req.body.title;
-    const price = req.body.price;
-    if (!req.session.cart[menuItem]) {
-      req.session.cart[menuItem] = {
-        num: 1,
-        price: Number(price)
-      }
-    } else {
-      req.session.cart[menuItem].num += 1;
-      req.session.cart[menuItem].price = Number(req.session.cart[menuItem].price) + Number(price);
-    }
-    res.status(200).json(req.session.cart);
+    //
   });
   return cartRoutes;
 };
