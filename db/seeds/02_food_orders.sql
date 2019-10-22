e7bb55da118f2c06c925a0e522683362a8b0f5e0
charcharmasonjar <charlotte.dibb@gmail.com>
2019-10-22 18:18:36 +0000

    Create some food orders for users

--- db/seeds/02_food_orders.sql
+++ db/seeds/02_food_orders.sql
@@ -2,3 +2,11 @@
 
 INSERT INTO food_orders (user_id, created_at, updated_at, completed_at, comments)
   VALUES (1, NOW(), NOW(), NOW(), 'better be good');
+INSERT INTO food_orders (user_id, created_at)
+  VALUES (2, NOW());
+INSERT INTO food_orders (user_id, created_at)
+  VALUES (3, NOW());
+INSERT INTO food_orders (user_id, created_at)
+  VALUES (4, NOW());
+INSERT INTO food_orders (user_id, created_at)
+  VALUES (5, NOW());
