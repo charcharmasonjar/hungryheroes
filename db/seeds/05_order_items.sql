4b51f18f6b5a32e109bcb82a6fe849117be5aff0
charcharmasonjar <charlotte.dibb@gmail.com>
2019-10-22 18:19:11 +0000

    Add some order items to food orders

--- db/seeds/05_order_items.sql
+++ db/seeds/05_order_items.sql
@@ -1,5 +1,12 @@
--- Widgets table seeds here (Example)
-INSERT INTO
-  order_items (food_order_id, menu_item_id)
-VALUES
-  (1, 1);
+-- order-items table seeds here
+
+-- order numbah one
+INSERT INTO order_items (food_order_id, menu_item_id) VALUES (1, 1);
+INSERT INTO order_items (food_order_id, menu_item_id) VALUES (1, 2);
+INSERT INTO order_items (food_order_id, menu_item_id) VALUES (1, 3);
+INSERT INTO order_items (food_order_id, menu_item_id) VALUES (1, 4);
+
+-- order numbah two
+INSERT INTO order_items (food_order_id, menu_item_id) VALUES (2, 5);
+INSERT INTO order_items (food_order_id, menu_item_id) VALUES (2, 6);
+INSERT INTO order_items (food_order_id, menu_item_id) VALUES (2, 7);
