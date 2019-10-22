983d1648cfa285ea27b728ee0d1b0c6e253a1de4
charcharmasonjar <charlotte.dibb@gmail.com>
2019-10-22 18:43:23 +0000

    Update cart object saved in cookie

--- routes/cart.js
+++ routes/cart.js
@@ -5,13 +5,38 @@
 
 const express = require('express');
 const cartRoutes = express.Router();
+const cookieSession = require('cookie-session');
+const app = express();
+
+app.set('trust proxy', 1) // trust first proxy
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2']
+}))
+
+
 
 module.exports = (dbHelpers) => {
   cartRoutes.post("/", (req, res) => {
-    if (!req.body.text) {
-      res.status(400).json({ error: 'invalid request: no data in POST body'});
-      return;
+    // testing if getOrderInfo function is working
+    // dbHelpers.getOrderInfo(1).then (res => {
+    //   console.log(res);
+    // })
+
+    const menuItem = req.body.title;
+    const price = req.body.price;
+    if (!req.session.cart[menuItem]) {
+      req.session.cart[menuItem] = {
+        num: 1,
+        price: Number(price)
+      }
+    } else {
+      req.session.cart[menuItem].num += 1;
+      req.session.cart[menuItem].price = Number(req.session.cart[menuItem].price) + Number(price);
     }
+    res.status(200).json(req.session.cart);
+
   });
   return cartRoutes;
 };
