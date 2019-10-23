6cb07edf1caf1380f65f0c93afaf08d3b6ee3bb9
charcharmasonjar <charlotte.dibb@gmail.com>
2019-10-23 06:12:15 +0000

    Toggle cart body when cart header is clicked

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -2,9 +2,16 @@ const cart = {};
 
 $(document).ready(function () {
 
+  //cart-body when cart header is clicked
+  $(".cart-header").click(() => {
+    $(".hide-cart").slideToggle("slow", () => {
+      //done
+    });
+  })
+
   //toggles write payment form when add payment button is clicked
   $("#add-payment").click(() => {
-    $(".payment-container").slideToggle("fast", () => {
+    $(".payment-container").slideToggle("slow", () => {
       //done
     });
   })
