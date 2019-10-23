75e7e993bfd05d1513aaeb336b4ea58090c75e6a
charcharmasonjar <charlotte.dibb@gmail.com>
2019-10-23 01:27:29 +0000

    Remove unnecessary jquery - no longer need to prevent form submission as the button doesn't submit the form

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -2,17 +2,6 @@ const cart = {};
 
 $(document).ready(function () {
 
-  
-
-  $(".add-order").submit(function (event) {
-    event.preventDefault();
-    console.log("default prevented");
-
-
-
-  })
-
-
   //toggles write payment form when add payment button is clicked
   $("#add-payment").click(() => {
     $(".payment-container").slideToggle("fast", () => {
