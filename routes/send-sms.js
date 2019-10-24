7a40162aaa5d87e9c0ba0760bbc7f707833bf691
johnnyb88 <johnnybarratt@gmail.com>
2019-10-24 20:10:25 +0000

    implement db helper function to access restaurant user response data

---
+++ routes/send-sms.js
@@ -0,0 +1,50 @@
+
+const express = require('express');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const router = express.Router();
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const twilioPhone = process.env.TWILIO_NUMBER;
+const restaurantPhone = process.env.RESTAURANT_NUMBER;
+const client = require('twilio')(accountSid, authToken);
+
+
+module.exports = (dbHelpers) => {
+
+  router.post("/", (req, res) => {
+    const twiml = new MessagingResponse();
+    let resResponse = req.body.Body;
+    let response = resResponse.split(" ");
+    let orderId = response[0];
+    let prepTime = response[1];
+
+
+    dbHelpers.getUserNameAndPrepTime(orderId)
+      .then((res) => {
+        console.log(res);
+        let name = res.rows[0].first_name;
+        let custPhone = res.rows[0].phone;
+        // let prepTime += res.rows[0].prep_time;
+        client.messages
+          .create({
+            from: twilioPhone,
+            body: `Hi ${name}! Thanks for ordering with HUNGRY HEROES. Your food is ready for pickup!`,
+            to: `+1${custPhone}`
+          })
+          .then(message => console.log(message.sid))
+          .catch(err => {
+            console.log(err);
+          })
+          .done();
+
+      });
+    twiml.message('Message sent to customer!');
+    res.writeHead(200, {'Content-type': 'text/xml'});
+    res.end(twiml.toString());
+  });
+
+
+  return router;
+
+};
+
