724dabeb4c9dfc77577d8f941692df03f28eec78
charcharmasonjar <charlotte.dibb@gmail.com>
2019-10-24 01:04:28 +0000

    Remove twilio related code that is not being used in this file

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -17,39 +17,7 @@ $(document).ready(function () {
   })
 
   $('#checkout').click(() => {
-    $.ajax({ method: 'POST', url: '/cart', data: cart})})
+    $.ajax({ method: 'POST', url: '/cart', data: cart })
+  })
 });
 
-const twilioOrder = {
-  name: $name,
-  phoneNumber: users.phone,
-  orderItems: finalOrderObj.quantity_of_items,
-  totalPrice: $totalPrice,
-  comments: $comments
-}
-
-if ($name && users.phone) {
-
-  $.ajax({
-    method: "POST",
-    url: "/api/users",
-    data: user
-  })
-    .done(function(id) {
-      finalOrderObj['user_id'] = id[0];
-      $.ajax({
-        method: "POST",
-        url: "/api/orders",
-        data: finalOrderObj
-      })
-        .done(function(orderId) {
-          twilioOrder['orderId'] = orderId[0];
-          $.ajax({
-            method: "POST",
-            url: "/sms",
-            data: twilioOrder
-          });
-        });
-    });
-
-}
