9a0ab12584e6cd510d215891317ce9d157bfab6b
johnnyb88 <johnnybarratt@gmail.com>
2019-10-24 01:17:39 +0000

    add routes to server

--- routes/cart.js
+++ routes/cart.js
@@ -10,7 +10,7 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const twilioPhone = process.env.TWILIO_NUMBER;
 const client = require('twilio')(accountSid, authToken);
-
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 module.exports = (dbHelpers) => {
 
 
@@ -87,7 +87,6 @@ module.exports = (dbHelpers) => {
       })
 
   });
-
 
   return cartRoutes;
 };
