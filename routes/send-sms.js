c16ab5b1e2994bb3f240741f75694cb67c6fc157
johnnyb88 <johnnybarratt@gmail.com>
2019-10-24 21:30:15 +0000

    create 2 messages from restaurant conditional on the boolean status of the food order

--- routes/send-sms.js
+++ routes/send-sms.js
@@ -11,34 +11,53 @@ const client = require('twilio')(accountSid, authToken);
 
 module.exports = (dbHelpers) => {
 
-
+  // ----- triggered by sms from restaurant ----- //
   router.post("/", (req, res) => {
     const twiml = new MessagingResponse();
     let resResponse = req.body.Body;
     let response = resResponse.split(" ");
     let orderId = response[0];
     let prepTime = Number(response[1]);
+    let isReady = response[2];
 
 
     dbHelpers.getUserNameAndPrepTime(orderId)
       .then((res) => {
-        console.log(res);
+
         let name = res.rows[0].first_name;
         let custPhone = res.rows[0].phone;
-        prepTime += Number(res.rows[0].prep_time);
-        client.messages
-          .create({
-            from: twilioPhone,
-            body: `Hi ${name}! Thanks for ordering with HUNGRY HEROES. Your estimated order time is ${prepTime} minutes.`,
-            to: `+1${custPhone}`
-          })
-          .then(message => console.log(message.sid))
-          .catch(err => {
-            console.log(err);
-          })
-          .done();
 
+        // ----- checks to see if the order status is ready. If not, send first sms with additional prep time needed ----- //
+        if (isReady === 'false') {
+          prepTime += Number(res.rows[0].prep_time);
+          client.messages
+            .create({
+              from: twilioPhone,
+              body: `Hi ${name}! Thanks for ordering with HUNGRY HEROES. Your estimated order time is ${prepTime} minutes.`,
+              to: `+1${custPhone}`
+            })
+            .then(message => console.log(message.sid))
+            .catch(err => {
+              console.log(err);
+            });
+
+
+        // ----- order is ready for pick up ----- //
+        } else if (isReady === 'true') {
+          client.messages
+            .create({
+              from: twilioPhone,
+              body: `Hi ${name}! Your food order from HUNGRY HEROES is ready for pickup. `,
+              to: `+1${custPhone}`
+            })
+            .then(message => console.log(message.sid))
+            .catch(err => {
+              console.log(err);
+            });
+
+        }
       });
+    // ----- confirms to restaurant that customer recieved message ----- //
     twiml.message('Message sent to customer!');
     res.writeHead(200, {'Content-type': 'text/xml'});
     res.end(twiml.toString());
